3290ad975cda563994538b726435506fd663f548
Abbas Hooshmand <abbas.hooshmand@gmail.com>
2018-01-22 12:18:07 -0500

    Finished Section 3

---
+++ TotalSpentByCustomer.py
@@ -0,0 +1,23 @@
+import findspark
+findspark.init()
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("SpendByCustomer")
+sc = SparkContext(conf=conf)
+
+
+def extractCustomerPricePairs(line):
+    fields = line.split(',')
+    return (int(fields[0]), float(fields[2]))
+
+
+input = sc.textFile("file:///Users/abbas/Documents/Projects_Repositories/SparkPython/customer-orders.csv")
+
+mappedInput = input.map(extractCustomerPricePairs)
+
+totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y)
+
+results = totalByCustomer.collect()
+
+for result in results:
+    print(result)
--- WordCountBetterSorted.py
+++ WordCountBetterSorted.py
@@ -3,11 +3,13 @@ findspark.init()
 import re
 from pyspark import SparkConf, SparkContext
 
+
 def normalizeWords(text):
     return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
+
 conf = SparkConf().setMaster("local").setAppName("WordCount")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
 input = sc.textFile("file:///Users/abbas/Documents/Projects_Repositories/SparkPython/book.txt")
 words = input.flatMap(normalizeWords)
